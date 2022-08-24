4cd4c82463b7d75a48fd53929f4d7ed7da9882aa
j2k <jakalogin1808@gmail.com>
2022-08-25 00:49:11 +0400

    added simple GUI (activity_main.xml|MainActivity.kt|strings.xml)

--- app/src/main/java/com/j2k/designkeyboard/MainActivity.kt
+++ app/src/main/java/com/j2k/designkeyboard/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.j2k.designkeyboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openSettings(view: View) {
+        startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
     }
 }
